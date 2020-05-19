2c777b6296ba5140179eb4e9bac83fde95e55ad8
Hmwoodw <Hwoodward@csub.edu>
2020-05-18 17:34:30 -0700

    Neon package

---
+++ UCE. Casual Game/Assets/Scripts/HelloWorld.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <para>MonoBehaviour is the base class from which every Unity script derives.</para>
+public class HelloWorld : MonoBehaviour
+{
+    public HelloWorld()
+    {
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
